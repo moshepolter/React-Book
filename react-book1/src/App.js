d6eee7cd400294b8264d64ae302681371a861d5c
moshepolter <moshepolter@outlook.com>
2020-08-31 18:47:28 -0400

    added todo Items...

--- react-book1/src/App.js
+++ react-book1/src/App.js
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+export default class App extends React.Component{
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  constructor(props){
+    super(props);
+    this.state = {
+      userName:"Adam",
+      todoItems: [{
+        action: "buyflowers", done: false,
+        action: "get shoes", done: false,
+        action: "collect tickets", done: false,
+        action: "call joe", done: false,
+      }],
+    }
+  }
+  updateNewTextValue = (event) =>{
+    this.setState ({newItemText: event.target.value})
+  }
+  creatNewTodo = () =>{
+    if(!this.state.todoItems
+      .find(item => item.action === this.state.newItemText)){
+        this.setState({
+          todoItems: [...this.state.todoItems,
+            {action: this.state.newItemText, done: false}],
+            newItemText: ""
+        })
+    }
+  }
+  render(){
+    return(
+      <div>
+        <h4 className="bg-primary text-white text-center p-2">
+          {this.state.userName}`s To Do List
+          ({this.state.todoItems.filter(t => !t.done).length} item(s) to do)
+        </h4>
+      <button className="btn btn-primary m-2" 
+        onClick={this.creatNewTodo}>
+        Add
+      </button>
     </div>
-  );
+    )
+  }
 }
 
-export default App;
+
+
