bcf96a57dd5d2a803ff975727ca1e8f637c71ec7
moshepolter <moshepolter@outlook.com>
2020-09-02 18:45:25 -0400

    finished todo list

--- react-book1/src/App.js
+++ react-book1/src/App.js
@@ -26,6 +26,18 @@ export default class App extends React.Component{
         })
     }
   }
+  toggleTodo = (todo) => this.setState({ todoItems:
+  this.state.todoItems.map(item => item.action === todo.action
+    ? {...item, done:!item.done}: item) });
+    todoTableRows = () => this.state.todoItems.map(item =>
+      <tr key={item.action}>
+        <td>{item.action}</td>
+        <td>
+          <input type="checkbox" checked={item.done}
+          onChange={() => this.toggleTodo(item)} />
+        </td>
+      </tr>);
+
   render(){
     return(
       <div>
@@ -43,6 +55,19 @@ export default class App extends React.Component{
           Add
         </button>
         </div>
+        <table className="table table-stripped table-borderd">
+          <thead>
+            <tr>
+              <th>
+                Description
+              </th>
+              <th>Done</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.todoTableRows() }
+          </tbody>
+          </table>
       </div>
     </div>
     )
